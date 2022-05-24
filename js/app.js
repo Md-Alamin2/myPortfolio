2b5002c55a8e76cdc080cf53a0951b007edd0c06
Md-Alamin2 <mdalaminn8733@gmail.com>
2022-05-24 20:08:02 +0600

    update

--- js/app.js
+++ js/app.js
@@ -36,3 +36,40 @@ $(document).ready(function(){
     });
   
   });
+
+function sendContact(){
+  // const submitButton = document.getElementById('submit-btn');
+  // submitButton.disabled = true;
+
+  const contactForm = document.getElementById('contact-form');
+  const data = new URLSearchParams();
+  for(const pair of new FormData(contactForm)){
+    data.append(pair[0], pair[1]);
+  }
+  console.log(data)
+const url =`https://formsubmit.co/mdalaminn8733@gmail.com`;
+
+fetch(url, {
+  method: `post`, 
+  body: data
+})
+.then(res => {
+  Swal.fire({
+    icon: 'success',
+    title: 'Thank You',
+    showConfirmButton: true,
+    
+  })
+  // submitButton.disabled = false;
+  contactForm.reset();
+  
+})
+
+}
+// error pop 
+/*Swal.fire({
+  title: 'Error!',
+  text: 'Do you want to continue',
+  icon: 'error',
+  confirmButtonText: 'Cool'
+})*/
